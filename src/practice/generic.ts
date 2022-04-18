e6dc8f38bb44ac0fe71c241bd9a51e2bb0bfdcd7
songmin <tft0720@gmail.com>
2022-04-18 21:02:25 +0900

    제네릭 조건

--- src/practice/generic.ts
+++ src/practice/generic.ts
@@ -14,3 +14,58 @@ class SimpleEither<L, R> implements Either<L, R> {
 }
 
 const either = new SimpleEither('Hello', 123);
+
+/** Generic Constrains */
+
+interface Employee {
+  pay(): void;
+}
+
+class FullTimeEmployee implements Employee {
+  pay() {
+    console.log('full time!!');
+  }
+  workFullTime() {}
+}
+
+class PartTimeEmployee implements Employee {
+  pay() {
+    console.log('part time!!');
+  }
+  workPartTime() {}
+}
+
+function payBad(employee: Employee): Employee {
+  employee.pay();
+  return employee;
+}
+
+function payGood<T extends Employee>(employee: T): T {
+  employee.pay();
+  return employee;
+}
+
+const ivan = new FullTimeEmployee();
+const ivanAfterPay = payGood(ivan);
+ivanAfterPay.workFullTime();
+
+const selah = new PartTimeEmployee();
+const selahAfterPay = payGood(selah);
+selahAfterPay.workPartTime();
+
+const obj1 = {
+  name: 'ivanselah',
+  age: 34,
+};
+
+const obj2 = {
+  fruit: '🍎',
+};
+
+function getValue<O, K extends keyof O>(obj: O, key: K): O[K] {
+  return obj[key];
+}
+
+console.log(getValue(obj1, 'name'));
+console.log(getValue(obj1, 'age'));
+console.log(getValue(obj2, 'fruit'));
